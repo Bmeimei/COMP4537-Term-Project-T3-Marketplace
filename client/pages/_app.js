16e401f265f2fdbc110fadf10c07ef1ba4127a65
Luke Mei <skystar505836915@gmail.com>
2021-11-01 14:53:50 -0700

    upload dependencies

---
+++ client/pages/_app.js
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Online Marketplace</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
