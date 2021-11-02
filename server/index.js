29603fbc2d05ac0cf8baa23024689fcb73348955
Luke Mei <skystar505836915@gmail.com>
2021-11-01 21:34:49 -0700

    initialize express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Fine
+const OK = 200;
+
+// Miss Params like word or definition
+const BAD_REQUEST = 400;
+
+// Server get the problems
+const INTERNAL_SERVER_ERROR = 500;
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*"
+  })
+);
+
+app.get("/", (req, res) => {
+  res.status(OK).send({
+    message: "Hey man!"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT: ${PORT}`);
+});
