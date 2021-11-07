4086d6047e0a263c2315e4d5f0db67a013b8b7c0
Luke Mei <skystar505836915@gmail.com>
2021-11-06 23:06:55 -0700

    initialize the documentation page

---
+++ client/pages/documentation.js
@@ -0,0 +1,8 @@
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+const Documentation = () => {
+  return <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />;
+};
+
+export default Documentation;
