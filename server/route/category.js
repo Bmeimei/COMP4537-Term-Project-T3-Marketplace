e95c93c20a19f1cd5aafeefb66255102f220baa0
Luke Mei <skystar505836915@gmail.com>
2021-11-17 00:26:14 -0800

    add endpoint for category

--- server/route/category.js
+++ server/route/category.js
@@ -3,6 +3,11 @@ import { addCategory, getAllCategory } from "../controller/category.js";
 
 const categoryRouter = express.Router();
 
+categoryRouter.use((req, res, next) => {
+  req.endpoint = "/category";
+  next();
+});
+
 categoryRouter.get("/", getAllCategory);
 categoryRouter.post("/", addCategory);
 
