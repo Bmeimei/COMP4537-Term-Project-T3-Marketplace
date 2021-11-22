ca3ec36556c66e2bef857d17e82d6ea690688c4d
Luke Mei <skystar505836915@gmail.com>
2021-11-21 21:12:01 -0800

    add login page and login logic

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,11 +1,21 @@
 import axios from "axios";
-import baseRequest from "./baseRequest";
+import { userBaseRequest } from "./baseRequest";
 
 export const loginRequest = async (email, password) => {
-    return await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/login`, {
-        email,
-        password
-    });
+  return await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/login`, {
+    email,
+    password
+  });
 };
 
-export const getEndpoint = () => baseRequest.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/endpoint`);
+export const signupRequest = async (username, email, password) => {
+  return await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/signup`, {
+    username,
+    email,
+    password
+  });
+};
+
+export const getCurrentUser = () => {
+  return userBaseRequest(`${process.env.NEXT_PUBLIC_SERVER_URL}/user`);
+};
