f022caa61a645b276825197fe41927c33cb833b9
Luke Mei <skystar505836915@gmail.com>
2021-11-27 21:27:19 -0800

    try the cors again

--- server/index.js
+++ server/index.js
@@ -18,7 +18,11 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_PORT
+  })
+);
 
 mongoose
   .connect(process.env.MONGODB_URL, {
