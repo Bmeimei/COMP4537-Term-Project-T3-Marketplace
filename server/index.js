de19eee70384d2071bde299985b62ea3d8b41b7b
Luke Mei <skystar505836915@gmail.com>
2021-11-27 21:31:54 -0800

    change endpoint

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://marketplace.cacodehunter.com");
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Authorization");
   res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
   next();
